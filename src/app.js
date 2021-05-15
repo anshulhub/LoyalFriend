f017ff3b819dca2c202bbc092569894fec0a5bc7
Anshul <anshulgarg712000@gmail.com>
2021-05-15 17:20:41 +0530

    added error pages

--- src/app.js
+++ src/app.js
@@ -4,96 +4,101 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-const db= require("./db/conn");
+const db = require("./db/conn");
 const Register = require("./models/registers")
 const hbs = require("hbs");
-const bcrypt  = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 
-const static_path= path.join(__dirname,"../public");
-const view_path= path.join(__dirname,"../templates/views");
-const partials_path= path.join(__dirname,"../templates/partials");
+const static_path = path.join(__dirname, "../public");
+const view_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.static(static_path));
-app.set("view engine" , "hbs");
-app.set("views",view_path);
+app.set("view engine", "hbs");
+app.set("views", view_path);
 hbs.registerPartials(partials_path);
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/",(req,res) => {
-res.render("login");
+
+app.get("/", (req, res) => {
+    res.render("login");
 })
-app.get("/login",(req,res) => {
-res.render("login");
+app.get("/login", (req, res) => {
+    res.render("login");
 })
-app.get("/register",(req,res) => {
-res.render("register");
+app.get("/register", (req, res) => {
+    res.render("register");
 })
-app.get("/index",(req,res) => {
-res.render("index");
+app.get("/index", (req, res) => {
+    res.render("index");
 })
-app.get("/about",(req,res) => {
-res.render("about");
+app.get("/about", (req, res) => {
+    res.render("about");
 })
-app.get("/breed/german",(req,res) => {
-res.render("breed/german");
+app.get("/breed/german", (req, res) => {
+    res.render("breed/german");
 })
-app.get("/breed/labrador",(req,res) => {
-res.render("breed/labrador");
+app.get("/breed/labrador", (req, res) => {
+    res.render("breed/labrador");
 })
-app.get("/organisation/posh",(req,res) => {
-res.render("organisation/posh");
+app.get("/organisation/posh", (req, res) => {
+    res.render("organisation/posh");
 })
-app.get("/organisation/apolo",(req,res) => {
-res.render("organisation/apolo");
+app.get("/organisation/apolo", (req, res) => {
+    res.render("organisation/apolo");
 })
 
-app.post("/register", async (req,res) => {
-    try{
+
+app.post("/register", async (req, res) => {
+    try {
         const registerUser = new Register({
-            username : req.body.username,
-            phone : req.body.number,
-            email : req.body.email,
-            password : req.body.password
+            username: req.body.username,
+            phone: req.body.number,
+            email: req.body.email,
+            password: req.body.password
         })
 
-       const registered = await registerUser.save();
-       
-       res.status(201).render("login");
-       
+        const registered = await registerUser.save();
+
+        res.status(201).render("login");
+
 
     } catch (error) {
         res.status(400).send(error);
     }
 })
 
-app.post("/login",async (req,res) => {
-    try{
-        const email = req.body.email;
-        const password=req.body.password;
-        const loginemail = await Register.findOne({email : email});
-        const loginpass = await bcrypt.compare(password,loginemail.password);
 
-        if(loginpass){
+app.post("/login", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const loginemail = await Register.findOne({ email: email });
+        const loginpass = await bcrypt.compare(password, loginemail.password);
+
+        if (loginpass) {
             res.status(201).render("index");
         }
-        else{
-            res.send("Invalid details Entered"); 
+        else {
+            res.send("Invalid details Entered");
         }
 
-    }catch(err){
-         res.status(400).send(err);
+    } catch (err) {
+        res.status(400).send(err);
     }
-    })
+})
 
-app.get("/",(req,res) => {
-res.send("Something's Missing!!!");
+
+app.get("*", (req, res) => {
+    res.render('404page', {
+        errorMsg: "Opps! page not found, Click Here to go back"
+    })
 })
 
 
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
